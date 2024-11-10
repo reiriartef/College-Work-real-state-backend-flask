7fa285a32318bfbee53b5874228044b4cb9d89f8
Ricardo Iriarte <95953488+reiriartef@users.noreply.github.com>
2024-11-10 15:12:10 -0400

    Actualizar db_config.py

--- db_config.py
+++ db_config.py
@@ -4,8 +4,8 @@ import psycopg2
 def get_db_connection():
     conn = psycopg2.connect(
         dbname="inmobiliaria",
-        user="postgres",
-        password="LordC",
+        user="",
+        password="",
         host="localhost",
         port="5432",
     )
